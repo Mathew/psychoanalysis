03a55d5200f6a1f7a29dc147bc65f1ba7fe8b89c
Martin McNickle <martin.mcnickle@fanduel.com>
2013-03-23 14:52:24 +0000

    Adding day entry mockup.

--- psychoanalysis/urls.py
+++ psychoanalysis/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import patterns, include, url
-
+from django.views.generic.base import TemplateView
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
 # admin.autodiscover()
@@ -14,4 +14,5 @@ urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
     # url(r'^admin/', include(admin.site.urls)),
+    url(r'^day_entry_mockup/$', TemplateView.as_view(template_name='day_entry_mockup.html'))
 )
