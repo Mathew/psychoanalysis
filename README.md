58149c095e5ae800d24d23087027c7a99615ae9e
Mathew Taylor <mat@mathewtaylor.co.uk>
2013-03-23 12:58:57 +0000

    add django project, add settings layout

--- README.md
+++ README.md
@@ -2,3 +2,8 @@ psychoanalysis
 ==============
 
 NHS Hack Day project.
+
+Commands
+------
+
+python manage.py runserver 0.0.0.0:8000 --settings=psychoanalysis.settings.development
---
+++ psychoanalysis/settings/development.py
@@ -0,0 +1,30 @@
+from .base import *
+
+DEBUG = TEMPLATE_DEBUG = True
+
+INSTALLED_APPS = INSTALLED_APPS + (
+    'debug_toolbar',
+)
+
+MIDDLEWARE_CLASSES = MIDDLEWARE_CLASSES + (
+    #'debug_toolbar.middleware.DebugToolbarMiddleware',
+)
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',  # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'psychoanalysis',                      # Or path to database file if using sqlite3.
+        # The following settings are not used with sqlite3:
+        'USER': 'psychoanalysis',
+        'PASSWORD': 'psychoanalysis',
+        'HOST': '',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
+        'PORT': '',                      # Set to empty string for default.
+    }
+}
+
+# Debug Toolbar IPs.
+INTERNAL_IPS = (
+    '33.33.33.1',
+    '33.33.33.10',
+    '127.0.0.1',
+)
